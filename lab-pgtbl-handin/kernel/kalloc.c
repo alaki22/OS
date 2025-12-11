d0d28a9662955e608452188fd10f5d3ba841b40f
alaki22 <145773296+alaki22@users.noreply.github.com>
2025-12-11 23:23:17 +0400

    Add files via upload
    added hw3 (pagetables)

---
+++ lab-pgtbl-handin/kernel/kalloc.c
@@ -0,0 +1,146 @@
+// Physical memory allocator, for user processes,
+// kernel stacks, page-table pages,
+// and pipe buffers. Allocates whole 4096-byte pages.
+
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "riscv.h"
+#include "defs.h"
+
+#define SUPERPGBEGIN  (end + (2 * 1024 * 1024)) 
+#define SUPERPGEND    (SUPERPGBEGIN + (4 * SUPERPGSIZE)) 
+
+
+void freerange(void *pa_start, void *pa_end);
+
+extern char end[]; // first address after kernel.
+                   // defined by kernel.ld.
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  struct run *freelist;
+  struct run *superfreelist; 
+} kmem;
+
+
+void
+superfree(void *pa)
+{
+  struct run *r;
+
+  if(((uint64)pa % SUPERPGSIZE) != 0)
+    panic("superfree");
+
+  r = (struct run*)pa;
+
+  acquire(&kmem.lock);
+  r->next = kmem.superfreelist;
+  kmem.superfreelist = r;
+  release(&kmem.lock);
+}
+
+
+
+
+void
+kinit()
+{
+  initlock(&kmem.lock, "kmem");
+  freerange(end,(void*)PHYSTOP);
+ 
+}
+
+void 
+freerange(void *pa_start, void *pa_end)
+{
+  char* p = (char*)PGROUNDUP((uint64)pa_start); 
+  int superpages = 0;
+  while (p + PGSIZE <= (char*)SUPERPGROUNDUP((uint64)p) && p + PGSIZE <= (char*)pa_end) {
+    kfree(p);
+    p += PGSIZE;
+  }
+ 
+  p = (char*)SUPERPGROUNDUP((uint64)p); 
+  while (p + SUPERPGSIZE <= (char*)pa_end && superpages < 10) {
+    superfree(p);  
+    p += SUPERPGSIZE;
+    superpages++;
+  }
+ 
+  while (p + PGSIZE <= (char*)pa_end) {
+    kfree(p);  
+    p += PGSIZE;
+  }
+}
+
+// Free the page of physical memory pointed at by pa,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(void *pa)
+{
+  struct run *r;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
+
+  r = (struct run*)pa;
+
+  acquire(&kmem.lock);
+  r->next = kmem.freelist;
+  kmem.freelist = r;
+  release(&kmem.lock);
+}
+
+void*
+superalloc(void)
+{
+  struct run *r;
+  //printf("super alloc\n");
+  acquire(&kmem.lock);
+  r = kmem.superfreelist;
+  if(r)
+    kmem.superfreelist = r->next;
+  release(&kmem.lock);
+
+   if(r)
+    memset((char*)r, 5, SUPERPGSIZE); // fill with junk
+  return (char*)r;
+}
+
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+void *
+kalloc(void)
+{
+  struct run *r;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  if(r)
+    kmem.freelist = r->next;
+  release(&kmem.lock);
+
+  if(r)
+    memset((char*)r, 5, PGSIZE); // fill with junk
+  return (void*)r;
+}
+
+uint64
+hassuperpg(void) {
+  acquire(&kmem.lock);
+  uint64 tmp = (uint64)kmem.superfreelist;
+  release(&kmem.lock);
+  return tmp;
+}
